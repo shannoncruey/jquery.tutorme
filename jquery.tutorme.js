23da6071f4423422b21ac965b537f1d18c9f9714
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-06-06 11:29:09 -0400

    Foundational progress
    * split out the 'init' from 'addbubbles' so pages can have multiple
    tutorials.
    * added an option to set the DOM id of the tutorme container.
    * added 'below' attachTo type.
    * fixed the 'up' arrow
    * xPos and yPos will now be an offset if used in conjunction with
    attachTo
    * a pages list is now available for each button.  Bubbles in a certain
    'page' can be shown/hidden with one call.
    * added showbubble and hidebubble functions
    * added showpage and hidepage functions
    * added hideallpages function

--- jquery.tutorme.js
+++ jquery.tutorme.js
@@ -1,14 +1,20 @@
 /**
-*  5-29-2014 Neal Shannon Cruey
-*  https://github.com/shannoncruey/jquery.tutorme
-*
-*  A framework for adding a tutorial overlay to a web page.
-*
-**/
+ *  5-29-2014 Neal Shannon Cruey
+ *  https://github.com/shannoncruey/jquery.tutorme
+ *
+ *  A framework for adding a tutorial overlay to a web page.
+ *
+ **/
 
 $.tutorme = {
-	init : function(bubbles, options) {
-		var $tut = $('<div id="tutorme" class="hidden tutorme"></div>');
+	id : "tutorme",
+	init : function(options) {
+		// if the tutorial_id is defined in the options.
+		if (options.id) {
+			this.id += "_" + options.id;
+		}
+
+		var $tut = $('<div id="' + this.id + '" class="hidden tutorme"></div>');
 
 		// bind keydown events
 		$(document).on("keydown.tutorme", function(e) {
@@ -19,25 +25,37 @@ $.tutorme = {
 		});
 
 		// the close button
-		$tut.append('<div id="tutorme-close">X</div>');
-		$("#tutorme-close").live("click", function() {
-			$("#tutorme").hide()
+		$tut.append('<div id="' + this.id + '-close" class="tutorme-close">X</div>');
+		$("#" + $.tutorme.id + "-close").live("click", function() {
+			$("#" + $.tutorme.id).hide()
 		});
 
 		// the overlay
-		$tut.append('<div id="tutorme-overlay"></div>');
+		$tut.append('<div id="' + $.tutorme.id + '-overlay" class="tutorme-overlay"></div>');
 
 		// this is the page title
 		// THE ABSOLUTE POSITIONING IS SCREWIG THINGS UP
 		// THIS IS COVERING UP THE CLOSE BUTTON
 		//$tut.append('<h2 id="tutorme-title">' + ((options.title) ? options.title : "") + '</h2>');
 
+		$("body").append($tut);
+
+	},
+	addbubbles : function(bubbles) {
 		$(bubbles).each(function(idx, bubble) {
 			var $bbl = $('<div class="tutorme-bubble"><div class="tutorme-bubble-title"></div><div class="tutorme-bubble-text"></div></div>');
 
 			// does it get an ID?
 			if (bubble.id) {
-				$bbl.attr("id", bubble.id);
+				$bbl.attr("id", "tut_" + bubble.id);
+			}
+
+			// pages are nothing more than additional classes we stick on the bubble
+			// that way the showpage and hidepage functions will know what do hide/show.
+			if (bubble.pages) {
+				$(bubble.pages).each(function(idx, pagename) {
+					$bbl.addClass("tut_page_" + pagename);
+				});
 			}
 
 			$bbl.find(".tutorme-bubble-title").text((bubble.title) ? bubble.title : "");
@@ -47,6 +65,8 @@ $.tutorme = {
 
 			// can we see it?
 			if (bubble.hidden === true) {
+				// all "hidden" bubbles get a special class.
+				$bbl.addClass("tut_hidden");
 				$bbl.hide();
 			}
 
@@ -60,7 +80,8 @@ $.tutorme = {
 						$bbl.append('<div class="arrow-right" />');
 						break;
 					case "up":
-						$bbl.append('<div class="arrow-up" />');
+						$bbl.prepend('<div class="arrow-up" />');
+						$bbl.find(".arrow-up").css("margin-left", (bubble.width / 2) - 20 + "px");
 						break;
 					case "down":
 						$bbl.append('<div class="arrow-down" />');
@@ -74,42 +95,68 @@ $.tutorme = {
 				switch (bubble.attachPosition) {
 					case "topright":
 						break;
+					case "bottom":
+						$bbl.css("top", at.offset().top + (at.height() + 20) + "px");
+						$bbl.css("left", (at.offset().left + (at.width() / 2)) - (bubble.width - (at.width() / 2)) + "px");
+						break;
 					case "right":
-						$bbl.css("top", at.offset().top + "px");
-						$bbl.css("left", at.offset().left + (at.width() + 20) + "px");
+						// in this case, if xPos or yPos were defined, they are interpreted as
+						// a SHIFT from the attachTo element.
+						var xShift = (bubble.xPos) ? bubble.xPos : 0;
+						var yShift = (bubble.yPos) ? bubble.yPos : 0;
+						
+						$bbl.css("top", at.offset().top + yShift + "px");
+						//();
+						$bbl.css("left", (at.offset().left + at.outerWidth()) + xShift + "px");
 						break;
 					// case "over":
 					// $bbl.css("top", at.offset().top + "px");
-					// $bbl.css("left", at.offset().left + (at.width() / 2) - $bbl.width + "px");
+					// $bbl.css("left", at.offset().left + (at.width() / 2) - bubble.width + "px");
 					// break;
 					default:
 						$bbl.css("top", at.offset().top + "px");
 						$bbl.css("left", at.offset().left + "px");
 				}
+			} else {
+				// no attachTo?  How about explicit coordinates?
+				if (bubble.xPos) {
+					$bbl.css("left", bubble.xPos + "px");
+				}
+				if (bubble.yPos) {
+					$bbl.css("top", bubble.yPos + "px");
+				}
 			}
-
-			// explicit coordinated override attachTo
-			if (bubble.xPos) {
-				$bbl.css("left", bubble.xPos + "px");
-			}
-			if (bubble.yPos) {
-				$bbl.css("top", bubble.yPos + "px");
-			}
-
 			// if clickThrough is enabled...
 			if (bubble.onClick) {
 				$bbl.css("cursor", "pointer");
 				$bbl.click(bubble.onClick);
 			}
 
-			$tut.append($bbl);
-			$("body").append($tut);
+			$("#" + $.tutorme.id).append($bbl);
 		});
 	},
 	show : function() {
-		$("#tutorme").show();
+		$("#" + this.id).show();
 	},
 	hide : function() {
-		$("#tutorme").hide();
+		$("#" + this.id).hide();
+	},
+	showbubble : function(id) {
+		$("#tut_" + id).show();
+	},
+	hidebubble : function(id) {
+		$("#tut_" + id).hide();
+	},
+	showpage : function(pagename) {
+		// show all bubbles associated with a certain page.
+		$(".tut_page_" + pagename).show();
+	},
+	hidepage : function(pagename) {
+		// utility function to hide all "hidden" bubbles.
+		$(".tut_page_" + pagename).hide();
+	},
+	hideallpages : function() {
+		// utility function to hide all "hidden" bubbles.
+		$("[class*=tut_page_]").hide();
 	}
 }
